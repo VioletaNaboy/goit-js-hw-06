9c6430de7764ae3f1ef34ef435c0b3791c794d03
VioletaNaboy <vnaboishchykova@gmail.com>
2023-04-26 23:58:37 +0200

    task6

---
+++ src/js/task-06.js
@@ -0,0 +1,9 @@
+const textInput = document.querySelector('#validation-input');
+function ckeckInput(event) {
+
+    if (event.currentTarget.value.length >= textInput.dataset.length) {
+        textInput.classList.remove("invalid");
+    textInput.classList.add("valid");}
+    else {   textInput.classList.remove("valid"); textInput.classList.add("invalid")}
+  };
+textInput.addEventListener("blur", ckeckInput );
