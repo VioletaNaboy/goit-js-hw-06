7673d09d00c6580ad2e3d5a6afa5566a8949f8c9
VioletaNaboy <vnaboishchykova@gmail.com>
2023-04-26 14:50:31 +0200

    +task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elAllIngredients = [];
+ingredients.forEach((ingredient) => {
+  const elIngredient = document.createElement("li");
+  elIngredient.textContent = ingredient;
+  elIngredient.class = "item";
+  elAllIngredients.push(elIngredient);
+});
+const elList = document.querySelector("#ingredients");
+elList.prepend(...elAllIngredients);
