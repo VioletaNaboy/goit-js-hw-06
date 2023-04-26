8fc9d48e87dd50e852efd6fd72044f905d8668f9
VioletaNaboy <vnaboishchykova@gmail.com>
2023-04-27 00:40:33 +0200

    task7

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector('#validation-input');
 function ckeckInput(event) {
 
-    if (event.currentTarget.value.length >= textInput.dataset.length) {
+    if (event.currentTarget.value.length === Number(textInput.dataset.length)) {
         textInput.classList.remove("invalid");
     textInput.classList.add("valid");}
     else {   textInput.classList.remove("valid"); textInput.classList.add("invalid")}
---
+++ src/js/task-07.js
@@ -0,0 +1,6 @@
+const sizeControl = document.querySelector("#font-size-control");
+const elText = document.querySelector("#text");
+sizeControl.addEventListener("input", (event) => { 
+    elText.style.fontSize = `${event.currentTarget.value}px`;
+   });
+// elem.getAttribute(name)
