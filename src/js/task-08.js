0232fe3b903af8704897ca97b3aa50261cea9eeb
VioletaNaboy <vnaboishchykova@gmail.com>
2023-04-27 14:01:11 +0200

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+    const {
+    elements: { email, password }
+  } = event.currentTarget;
+  
+    if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+    } else {
+        const formData = {
+            login: email.value,
+            password: password.value
+        };
+        console.log(formData);
+        form.reset();
+    }
+});
 
