62694776c70244e63e0886933ce718c8035360f2
VioletaNaboy <vnaboishchykova@gmail.com>
2023-04-27 15:43:58 +0200

    task10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const elBoxes = document.querySelector("#boxes");
+const elInput = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+function createBoxes() {
+  const amount = elInput.value;
+  for (let i = 1; i <= amount; i = i + 1) {
+    let elDiv = `<div style="background-color:${getRandomHexColor()}; width:${30+10*(i-1)}px; height:${30+10*(i-1)}px;"></div>`;
+    elBoxes.insertAdjacentHTML("afterbegin", elDiv);
+  }
+};
+function destroyBoxes() {
+  elBoxes.innerHTML = "";
+  elInput.value = "";
+};
+btnCreate.addEventListener("click", createBoxes);
+ btnDestroy.addEventListener("click", destroyBoxes);
